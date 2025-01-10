cb2fca7d6f5b6d25d86f3d7b0e0613581eb243cc
APapeIsName <gms72901217@gmail.com>
2025-01-10 16:39:26 +0900

    Initial Commit

---
+++ Umpa/UmpaApp.swift
@@ -0,0 +1,17 @@
+//
+//  UmpaApp.swift
+//  Umpa
+//
+//  Created by 공명선 on 1/10/25.
+//
+
+import SwiftUI
+
+@main
+struct UmpaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ UmpaTests/UmpaTests.swift
@@ -0,0 +1,17 @@
+//
+//  UmpaTests.swift
+//  UmpaTests
+//
+//  Created by 공명선 on 1/10/25.
+//
+
+import Testing
+@testable import Umpa
+
+struct UmpaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
