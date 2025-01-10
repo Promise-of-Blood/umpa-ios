b6b9116d44dec78359c8a7e7880075c02189cd12
APapeIsName <gms72901217@gmail.com>
2025-01-10 17:24:26 +0900

    Add: UI, Data Layer. LoginView.swift

---
+++ Umpa/UI/LoginView.swift
@@ -0,0 +1,18 @@
+//
+//  LoginView.swift
+//  Umpa
+//
+//  Created by 공명선 on 1/10/25.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoginView()
+}
