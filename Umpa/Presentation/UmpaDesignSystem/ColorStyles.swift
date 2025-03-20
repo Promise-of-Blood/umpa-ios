6f3fd407a4cd44b763ce65b28fd4312e06316fd1
Jaewon Yun <woin2ee@gmail.com>
2025-03-20 13:40:21 +0900

    Fix: 오타 수정

--- Umpa/Presentation/UmpaDesignSystem/ColorStyles.swift
+++ Umpa/Presentation/UmpaDesignSystem/ColorStyles.swift
@@ -3,7 +3,7 @@
 import SwiftUI
 
 enum UmpaColor {
-    static let main = Color(hex: "#5C79AF")
+    static let main = Color(hex: "5C79AF")
     static let darkGray = Color(hex: "72727C")
     static let lightBlue = Color(hex: "D6E1FF")
     static let lightGray = Color(hex: "9C9C9C")
