12329f01a233aecdf0904f3bbf79e458f1399fee
Jaewon Yun <woin2ee@gmail.com>
2025-03-24 17:03:14 +0900

    Comment: 개선을 위한 TODO 주석 추가

--- Umpa/Interactors/SignUpInteractor.swift
+++ Umpa/Interactors/SignUpInteractor.swift
@@ -12,6 +12,7 @@ struct DefaultSignUpInteractor: SignUpInteractor {
     @Injected(\.appState) private var appState
     @Injected(\.umpaApi) private var umpaApi
 
+    // TODO: 네트워크 요청 에러 시 처리 로직 추가 (Ex: Binding<Error?> 사용하여 Finish 화면의 error state 업데이트)
     func signUp(with model: SignUpModel) async {
         // TODO: Implement
 
