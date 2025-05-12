c12f1b7622dd14fd51ac3c8095550361f4f25e25
Jaewon Yun <woin2ee@gmail.com>
2025-05-12 23:39:46 +0900

    Style : 스타일 적용

--- Umpa/Application/UmpaApp.swift
+++ Umpa/Application/UmpaApp.swift
@@ -10,91 +10,91 @@ import SwiftUI
 
 @main
 struct UmpaApp: App {
-    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+  @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
 
-    @InjectedObject(\.appState) private var appState
+  @InjectedObject(\.appState) private var appState
 
-    #if DEBUG
-    @Injected(\.mockAppInteractor) // Preview 에도 사용됨
-    #else
-    @Injected(\.appInteractor)
-    #endif
-    private var appInteractor
+#if DEBUG
+  @Injected(\.mockAppInteractor) // Preview 에도 사용됨
+#else
+  @Injected(\.appInteractor)
+#endif
+  private var appInteractor
 
-    init() {
-        NetworkMonitor.shared.start()
-        prepareKakaoLogin()
-        prepareNaverLogin()
-        appInteractor.loadAppData()
+  init() {
+    NetworkMonitor.shared.start()
+    prepareKakaoLogin()
+    prepareNaverLogin()
+    appInteractor.loadAppData()
+  }
+
+  var body: some Scene {
+    WindowGroup {
+      mainWindow
+        .onOpenURL(perform: handleUrl)
+        .task(restorePreviousSignIn)
     }
+  }
 
-    var body: some Scene {
-        WindowGroup {
-            mainWindow
-                .onOpenURL(perform: handleUrl)
-                .task(restorePreviousSignIn)
-        }
+  @ViewBuilder
+  var mainWindow: some View {
+    if appState.system.isSplashFinished {
+      if appState.userData.loginInfo.isLoggedIn {
+        MainTabView()
+      } else {
+        LoginView()
+      }
+    } else {
+      SplashView()
     }
-
-    @ViewBuilder
-    var mainWindow: some View {
-        if appState.system.isSplashFinished {
-            if appState.userData.loginInfo.isLoggedIn {
-                MainTabView()
-            } else {
-                LoginView()
-            }
-        } else {
-            SplashView()
-        }
-    }
+  }
 }
 
 // MARK: - Private Methods
 
 extension UmpaApp {
-    private func prepareKakaoLogin() {
-        let appKey = Bundle.main.infoPlist.string(forKey: .kakaoNativeAppKey)
-        KakaoSDK.initSDK(appKey: appKey)
-    }
+  private func prepareKakaoLogin() {
+    let appKey = Bundle.main.infoPlist.string(forKey: .kakaoNativeAppKey)
+    KakaoSDK.initSDK(appKey: appKey)
+  }
 
-    private func prepareNaverLogin() {
-        NidOAuth.shared.initialize()
-        NidOAuth.shared.setLoginBehavior(.appPreferredWithInAppBrowserFallback)
-    }
+  private func prepareNaverLogin() {
+    NidOAuth.shared.initialize()
+    NidOAuth.shared.setLoginBehavior(.appPreferredWithInAppBrowserFallback)
+  }
 
-    private func handleUrl(_ url: URL) {
-        if AuthApi.isKakaoTalkLoginUrl(url) {
-            if !AuthController.handleOpenUrl(url: url) {
-                UmpaLogger.log("카카오 리다이렉트 URL 처리 실패", level: .error)
-            }
-            return
-        }
-        if NidOAuth.shared.handleURL(url) {
-            return
-        }
-        if GIDSignIn.sharedInstance.handle(url) {
-            return
-        }
+  private func handleUrl(_ url: URL) {
+    if AuthApi.isKakaoTalkLoginUrl(url) {
+      if !AuthController.handleOpenUrl(url: url) {
+        UmpaLogger.log("카카오 리다이렉트 URL 처리 실패", level: .error)
+      }
+      return
     }
+    if NidOAuth.shared.handleURL(url) {
+      return
+    }
+    if GIDSignIn.sharedInstance.handle(url) {
+      return
+    }
+  }
 
-    @Sendable
-    private func restorePreviousSignIn() async {
-        do {
-            let user = try await GIDSignIn.sharedInstance.restorePreviousSignIn()
-            // Check if `user` exists; otherwise, do something with `error`
-            UmpaLogger.log(user.description, level: .debug)
-        } catch {
-            UmpaLogger.log("Error restoring sign-in: \(error)", level: .error)
-        }
+  @Sendable
+  private func restorePreviousSignIn() async {
+    do {
+      let user = try await GIDSignIn.sharedInstance.restorePreviousSignIn()
+      // Check if `user` exists; otherwise, do something with `error`
+      UmpaLogger.log(user.description, level: .debug)
+    } catch {
+      UmpaLogger.log("Error restoring sign-in: \(error)", level: .error)
     }
+  }
 }
 
 class AppDelegate: NSObject, UIApplicationDelegate, ObservableObject {
-    func application(
-        _ application: UIApplication,
-        didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data
-    ) {
-        // Record the device token.
-    }
+  func application(
+    _ application: UIApplication,
+    didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data
+  ) {
+    // Record the device token.
+  }
 }
