62fc4c1f54a1d9c2913bc6108123005440419101
Jaewon Yun <woin2ee@gmail.com>
2025-05-14 22:06:10 +0900

    [UMPA-265] Implement : TeacherMainProfileCardView 컴포넌트 추가

--- Umpa/Presentation/Common/UmpaDesignSystem/ColorStyles.swift
+++ Umpa/Presentation/Common/UmpaDesignSystem/ColorStyles.swift
@@ -3,13 +3,15 @@
 import SwiftUI
 
 enum UmpaColor {
-    static let main = Color(hex: "5C79AF")
-    static let lightLightGray = Color(hex: "E8E8E8")
-    static let lightGray = Color(hex: "CCCCCC")
-    static let mediumGray = Color(hex: "767676")
-    static let darkGray = Color(hex: "363636")
-    static let lightBlue = Color(hex: "D6E1FF")
-    static let mainBlue = Color(hex: "3468F7")
-    static let darkBlue = Color(hex: "0932A1")
-    static let baseColor = Color(hex: "EFEFEF")
+  static let main = Color(hex: "5C79AF")
+  static let lightLightGray = Color(hex: "E8E8E8")
+  static let lightGray = Color(hex: "CCCCCC")
+  static let mediumGray = Color(hex: "767676")
+  static let darkGray = Color(hex: "363636")
+  static let lightBlue = Color(hex: "D6E1FF")
+  static let skyBlue = Color(hex: "4BA7FF")
+  static let mainBlue = Color(hex: "3468F7")
+  static let darkBlue = Color(hex: "0932A1")
+  static let baseColor = Color(hex: "EFEFEF")
+  static let gold = Color(hex: "FFD700")
 }
---
+++ Umpa/Presentation/MainTab/ProfileServiceManagement/Components/TeacherMainProfileCard/TeacherMainProfileCardView.swift
@@ -0,0 +1,129 @@
+// Created for Umpa in 2025
+
+import SwiftUI
+
+struct TeacherMainProfileCardView: View {
+  let model: Model
+
+  let editProfileAction: () -> Void
+  let previewProfileAction: () -> Void
+
+  var body: some View {
+    content
+  }
+
+  var content: some View {
+    VStack(spacing: fs(16)) {
+      header
+      experiences
+      bottomButtons
+    }
+    .padding(.horizontal, fs(16))
+    .padding(.vertical, fs(12))
+    .background(.white)
+    .clipShape(RoundedRectangle(cornerRadius: fs(10)))
+    .shadow(color: .black.opacity(0.1), radius: fs(10), y: fs(2))
+    .shadow(color: .black.opacity(0.15), radius: fs(4), y: fs(2))
+  }
+
+  var header: some View {
+    let imageSize: CGFloat = fs(50)
+    return HStack(spacing: fs(12)) {
+      AsyncImage(
+        url: URL(string: model.profileImageUrl)
+      ) { image in
+        image
+          .resizable()
+          .aspectRatio(contentMode: .fill)
+          .frame(width: imageSize, height: imageSize)
+          .clipShape(Circle())
+      } placeholder: {
+        ProgressView()
+          .progressViewStyle(.circular)
+          .frame(width: imageSize, height: imageSize)
+      }
+      VStack(spacing: fs(6)) {
+        Text(model.name)
+          .font(.pretendardSemiBold(size: fs(16)))
+          .foregroundStyle(.black)
+          .frame(maxWidth: .infinity, alignment: .leading)
+        Text(model.secondaryInfo)
+          .font(.pretendardRegular(size: fs(12)))
+          .foregroundStyle(UmpaColor.mediumGray)
+          .frame(maxWidth: .infinity, alignment: .leading)
+      }
+    }
+  }
+
+  var experiences: some View {
+    VStack(spacing: fs(6)) {
+      IndexingForEach(model.experiences) { _, experience in
+        HStack(spacing: fs(6)) {
+          Image(systemSymbol: .checkmarkCircleFill)
+            .foregroundStyle(UmpaColor.mainBlue)
+            .font(.system(size: 16, weight: .medium))
+          Text(experience)
+            .font(.pretendardRegular(size: fs(13)))
+            .frame(maxWidth: .infinity, alignment: .leading)
+            .lineLimit(1)
+        }
+      }
+    }
+    .frame(maxWidth: .infinity)
+  }
+
+  var bottomButtons: some View {
+    let cornerRadius: CGFloat = fs(8)
+    let verticalPadding: CGFloat = fs(8)
+    return HStack(spacing: fs(10)) {
+      Button(action: editProfileAction) {
+        Text("프로필 편집")
+          .font(.pretendardRegular(size: fs(13)))
+          .foregroundStyle(.black)
+          .frame(maxWidth: .infinity)
+          .padding(.vertical, verticalPadding)
+          .background(.white)
+          .innerRoundedStroke(UmpaColor.lightGray, cornerRadius: cornerRadius)
+          .clipShape(RoundedRectangle(cornerRadius: cornerRadius))
+      }
+      Button(action: previewProfileAction) {
+        Text("프로필 미리보기")
+          .font(.pretendardMedium(size: fs(13)))
+          .foregroundStyle(.white)
+          .padding(.vertical, verticalPadding)
+          .frame(maxWidth: .infinity)
+          .background(UmpaColor.skyBlue, in: RoundedRectangle(cornerRadius: cornerRadius))
+          .clipShape(RoundedRectangle(cornerRadius: cornerRadius))
+      }
+    }
+  }
+}
+
+extension TeacherMainProfileCardView {
+  struct Model {
+    let name: String
+    let profileImageUrl: String
+    let secondaryInfo: String
+    let experiences: [String]
+  }
+}
+
+#Preview {
+  TeacherMainProfileCardView(
+    model: .init(name: "장우영 선생님",
+                 profileImageUrl: "",
+                 secondaryInfo: "베이스 · 서울/연남동 ",
+                 experiences: [
+                   "호원대학교 베이스 전공 중퇴",
+                   "서울예술대학교, 서울예술대학교, 서울예술대학교, 서울예술대학교 베이스 전공 재학중",
+                   "AKMU - 롱디 녹음 세션",
+                 ]),
+    editProfileAction: {
+      print("Did Tap edit profile")
+    },
+    previewProfileAction: {
+      print("Did Tap preview profile")
+    },
+  )
+  .padding()
+}
