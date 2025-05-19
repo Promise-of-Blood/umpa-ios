979b828eee2f5da0f3c37dc3ab1f69ee8632cea7
Jaewon Yun <woin2ee@gmail.com>
2025-05-19 21:38:12 +0900

    [UMPA-268] Implement : 학생 카드 컴포넌트 구현

---
+++ Projects/Domain/Sources/Model/ValueObjects/LessonStatus.swift
@@ -0,0 +1,10 @@
+// Created for Umpa in 2025
+
+import Foundation
+
+public enum LessonStatus {
+  /// 진행중
+  case inProgress
+  /// 레슨 종료
+  case completed
+}
---
+++ Projects/Domain/Sources/Model/ValueObjects/PaymentType.swift
@@ -0,0 +1,10 @@
+// Created for Umpa in 2025
+
+import Foundation
+
+public enum PaymentType {
+  /// 직접 결제
+  case direct
+  /// 안심 결제
+  case safe
+}
