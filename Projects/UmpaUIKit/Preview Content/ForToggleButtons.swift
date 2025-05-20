1408e21beac157b5f2030d46f4a1a5bdbca6b796
Jaewon Yun <woin2ee@gmail.com>
2025-05-20 18:47:32 +0900

    [UMPA-280] Implement : ToggleButtons 구현

---
+++ Projects/UmpaUIKit/Preview Content/ForToggleButtons.swift
@@ -0,0 +1,9 @@
+// Created for Umpa in 2025
+
+import DeveloperToolsSupport
+import Foundation
+
+struct TestingToggleButtonsItem: ToggleButtons.SelectableItem {
+  let label: String
+  let image: ImageResource? = nil
+}
